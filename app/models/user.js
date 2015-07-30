027df3d21748135a2ccf553d7b11b6e81b3c7095
Hack Reactor Students <accounts@hackreactor.com>
2015-07-30 11:26:45 -0700

    realer than real deal holyfield

--- app/models/user.js
+++ app/models/user.js
@@ -33,6 +33,7 @@ db.userSchema.pre('save', function(next) {
   return cipher(this.get('password'), null, null).bind(this)
     .then(function(hash) {
       this.set('password', hash);
+      console.log("inside cipher callback")
       next();
     });
 })
